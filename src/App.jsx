5553a79f2561eb0081414deb765ac4404e21acb1
huzaifa311 <119949757+huzaifa311@users.noreply.github.com>
2023-09-12 14:10:26 -0700

    Add files via upload

--- src/App.jsx
+++ src/App.jsx
@@ -1,3 +1,4 @@
+//sir ka copy kia hua
 /* import { useState } from 'react'
 import './App.css'
 import Header from './Components/Header'
@@ -96,6 +97,262 @@ function App() {
 }
 export default App */
 
+//gpt ka 2sra
+/* import React, { useState } from "react";
+import Header from "../src/Components/Header";
+import Button from "../src/Components/Button";
+import { FooterWithSitemap } from "./Components/Footer";
+
+const App = () => {
+  const [todos, setTodos] = useState([]);
+  const [todoValue, setTodoValue] = useState("");
+  const [editTodoValue, setEditTodoValue] = useState("");
+
+  const addTodo = () => {
+    if (todoValue.trim() === "") {
+      alert("Cannot Add an Empty Todo");
+      return;
+    }
+    const todoObj = {
+      value: todoValue,
+      isEdit: false,
+    };
+    setTodos([...todos, todoObj]);
+    setTodoValue("");
+  };
+
+  const dltAll = () => {
+    setTodos([]);
+  };
+
+  const editTodo = (index) => {
+    const updatedTodos = todos.map((todo, i) => ({
+      ...todo,
+      isEdit: i === index,
+    }));
+    setTodos(updatedTodos);
+    setEditTodoValue(todos[index].value);
+  };
+
+  const dltTodo = (index) => {
+    const updatedTodos = [...todos.slice(0, index), ...todos.slice(index + 1)];
+    setTodos(updatedTodos);
+  };
+
+  const saveTodoValue = (index) => {
+    const updatedTodos = [...todos];
+    updatedTodos[index].value = editTodoValue;
+    updatedTodos[index].isEdit = false;
+    setTodos(updatedTodos);
+  };
+
+  return (
+    <div className="bg-gray-100 min-h-screen">
+      <Header />
+      <div className="container mx-auto p-8">
+        <div className="w-full md:w-1/2 mx-auto">
+          <input
+            className="h-14 w-full p-3 mb-4 rounded-lg border border-purple-950 outline-none"
+            placeholder="Add Todo"
+            type="text"
+            value={todoValue}
+            onChange={(e) => setTodoValue(e.target.value)}
+          />
+          <div className="flex space-x-2">
+            <Button
+              text="Add Todo"
+              clickTrigger={addTodo}
+              customClass="bg-green-500 hover:bg-green-700 flex-grow"
+            />
+            <Button
+              text="Delete All"
+              clickTrigger={dltAll}
+              customClass="bg-red-500 hover:bg-red-700 flex-grow"
+            />
+          </div>
+        </div>
+      </div>
+
+      <div className="container mx-auto">
+        <ul>
+          {todos.map((todo, index) => (
+            <li
+              key={index}
+              className="p-4 mb-4 rounded-lg border border-purple-600 bg-white flex flex-col justify-between items-center md:flex-row"
+            >
+              <div className="mb-4 md:mb-0">
+                {todo.value}
+              </div>
+              <div className="md:flex md:flex-col md:items-end">
+                {todo.isEdit ? (
+                  <div className="flex w-full space-x-2">
+                    <input
+                      className="h-11 w-full p-3 rounded-lg border border-purple-950 outline-none"
+                      type="text"
+                      onChange={(e) => setEditTodoValue(e.target.value)}
+                      value={editTodoValue}
+                    />
+                    <Button
+                      text="Save"
+                      clickTrigger={() => saveTodoValue(index)}
+                      customClass="bg-green-500 hover:bg-green-700"
+                    />
+                  </div>
+                ) : (
+                  <>
+                    <Button
+                      text="Edit"
+                      clickTrigger={() => editTodo(index)}
+                      customClass="bg-yellow-500 hover:bg-yellow-700"
+                    />
+                    <Button
+                      text="Delete"
+                      clickTrigger={() => dltTodo(index)}
+                      customClass="bg-red-500 hover:bg-red-700 mt-2"
+                    />
+                  </>
+                )}
+              </div>
+            </li>
+          ))}
+        </ul>
+      </div>
+
+      <FooterWithSitemap />
+    </div>
+  );
+};
+
+export default App; */
+
+// gpt ka pehla
+/* import React, { useState } from "react";
+import Header from "../src/Components/Header";
+import Button from "../src/Components/Button";
+import { FooterWithSitemap } from "./Components/Footer";
+import "./App.css";
+
+const App = () => {
+  const [todos, setTodos] = useState([]);
+  const [todoValue, setTodoValue] = useState("");
+  const [editTodoValue, setEditTodoValue] = useState("");
+
+  const addTodo = () => {
+    if (todoValue.trim() === "") {
+      alert("Cannot Add an Empty Todo");
+      return;
+    }
+    const todoObj = {
+      value: todoValue,
+      isEdit: false,
+    };
+    setTodos([...todos, todoObj]);
+    setTodoValue("");
+  };
+
+  const dltAll = () => {
+    setTodos([]);
+  };
+
+  const editTodo = (index) => {
+    const updatedTodos = todos.map((todo, i) => ({
+      ...todo,
+      isEdit: i === index,
+    }));
+    setTodos(updatedTodos);
+    setEditTodoValue(todos[index].value);
+  };
+
+  const dltTodo = (index) => {
+    const updatedTodos = [...todos.slice(0, index), ...todos.slice(index + 1)];
+    setTodos(updatedTodos);
+  };
+
+  const saveTodoValue = (index) => {
+    const updatedTodos = [...todos];
+    updatedTodos[index].value = editTodoValue;
+    updatedTodos[index].isEdit = false;
+    setTodos(updatedTodos);
+  };
+
+  return (
+    <div className="bg-gray-100 min-h-screen">
+      <Header />
+      <div className="container mx-auto p-8 mt-14">
+        <div className="w-full md:w-1/2 mx-auto">
+          <input
+            className="h-14 w-full p-3 mb-4 rounded-lg border border-purple-950 outline-none"
+            placeholder="Add Todo"
+            type="text"
+            value={todoValue}
+            onChange={(e) => setTodoValue(e.target.value)}
+          />
+          <div className="flex space-x-2">
+            <Button
+              text="Add Todo"
+              clickTrigger={addTodo}
+              customClass="bg-green-500 hover:bg-green-700 flex-grow"
+            />
+            <Button
+              text="Delete All"
+              clickTrigger={dltAll}
+              customClass="bg-red-500 hover:bg-red-700 flex-grow"
+            />
+          </div>
+        </div>
+      </div>
+
+      <div className="container mx-auto">
+        <ul>
+          {todos.map((todo, index) => (
+            <li
+              key={index}
+              className="p-4 mb-4 rounded-lg border border-purple-600 bg-white flex justify-between items-center"
+            >
+              {todo.isEdit ? (
+                <div className="flex w-full space-x-2">
+                  <input
+                    className="h-11 w-full p-3 rounded-lg border border-purple-950 outline-none"
+                    type="text"
+                    onChange={(e) => setEditTodoValue(e.target.value)}
+                    value={editTodoValue}
+                  />
+                  <Button
+                    text="Save"
+                    clickTrigger={() => saveTodoValue(index)}
+                    customClass="bg-green-500 hover:bg-green-700"
+                  />
+                </div>
+              ) : (
+                <>
+                  {todo.value}
+                  <div>
+                    <Button
+                      text="Edit"
+                      clickTrigger={() => editTodo(index)}
+                      customClass="bg-yellow-500 hover:bg-yellow-700"
+                    />
+                    <Button
+                      text="Delete"
+                      clickTrigger={() => dltTodo(index)}
+                      customClass="bg-red-500 hover:bg-red-700"
+                    />
+                  </div>
+                </>
+              )}
+            </li>
+          ))}
+        </ul>
+      </div>
+
+      <FooterWithSitemap />
+    </div>
+  );
+};
+
+export default App; */
+
+//mera apna
 import React from "react";
 import { useState } from "react";
 import "./App.css";
@@ -233,3 +490,5 @@ const App = () => {
 };
 
 export default App;
+
+
